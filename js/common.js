7ff501afc5577109f11e9cdf0fc7024632b11141
김성은 <kse012050@naver.com>
2023-09-20 03:26:07 +0900

    PC - 장애인 표준 사업장 인증 - 인증 자료 완료

--- js/common.js
+++ js/common.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
   // css index
 function styleIdx(){
   const selector = $('[data-styleIdx]');
+  selector.css('--totalIdx', selector.children().length - 1);
   selector.each(function(){
       const attrValue = $(this).attr('data-styleIdx');
       $(this).find('>' + (attrValue ? attrValue : ' *')).each(function(i){
