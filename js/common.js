00ac95cb0419e9996afffb1eeb04f35e838bf127
김성은 <kse012050@naver.com>
2023-09-19 16:07:09 +0900

    PC - 메인 페이지 완료

--- js/common.js
+++ js/common.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
-    var mainSwiper = new Swiper(".mainPage .topBox .mySwiper", {
-        pagination: {
-          el: ".mainPage .topBox .swiper-pagination",
-        },
-      });
+  // css index
+  $('[data-styleIdx]').length && styleIdx();
+
+  var mainSwiper = new Swiper(".mainPage .topBox .mySwiper", {
+    pagination: {
+      el: ".mainPage .topBox .swiper-pagination",
+    },
+  });
 })
+
+function styleIdx(){
+  const selector = $('[data-styleIdx]');
+  selector.each(function(){
+      const attrValue = $(this).attr('data-styleIdx');
+      $(this).find('>' + (attrValue ? attrValue : ' *')).each(function(i){
+          $(this).css('--styleIdx', i);
+      })
+  })
+}
